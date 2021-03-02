f63d4f5a9bcb63e904435c72cc65a3fe10208467
Rulin Tang <smiletrl@yahoo.com>
2021-03-02 18:46:37 +0800

    slice escape case

---
+++ pkg/escape/case5.go
@@ -0,0 +1,47 @@
+package escape
+
+import (
+	"fmt"
+	"time"
+)
+
+// content in slice has pointer or content value has pointer, the referenced variable escapes to heap.
+
+func case5() {
+	num := 42
+	fmt.Println(num)
+
+	emp := getEmployer5()
+	fmt.Println(emp)
+}
+
+type employer5 struct {
+	Name     string
+	Age      int
+	Title    *string
+	Birthday time.Time
+}
+
+//go:noinline
+func getEmployer5() int {
+	var names []*string
+	name := "adam" // moved to heap: title
+	for i := 0; i < 100; i++ {
+		names = append(names, &name)
+	}
+
+	var employers []employer5
+	title := "ceo" // moved to heap: title
+	for k := 0; k < 100; k++ {
+		employers = append(employers, employer5{
+			Title: &title,
+		})
+	}
+
+	var ages [80]*int
+	age := 12 // does not escape !!!
+	for j := 0; j < 80; j++ {
+		ages[j] = &age
+	}
+	return len(names) + len(employers) + len(ages)
+}
